648a00ba69cc936d0b4020295e0d17df8b73ea00
Alan Braithwaite <alan@runreveal.com>
2023-03-20 12:39:52 -0700

    new cli

--- Formula/runreveal.rb
+++ Formula/runreveal.rb
@@ -1,9 +1,9 @@
 class Runreveal < Formula
   desc 'RunReveal CLI'
   homepage 'https://runreveal.com'
-  url 'https://github.com/runreveal/homebrew-runreveal/releases/download/notasahi-1.1.1/runreveal-v0.1.3-darwin-arm64.tar.gz'
-  sha256 '7d0f295c366a95b3cee7e83bfb4be199189ae5ae63b0bc951a3e004cc94776c5'
-  version '0.1.3'
+  url 'https://github.com/runreveal/homebrew-runreveal/releases/download/runreveal-0.1.4/runreveal-v0.1.4-darwin-arm64.tar.gz'
+  sha256 '576f18dbb2dfbe155155432616b2f3bfe881d696e5133599b124a45b9b40d095'
+  version '0.1.4'
   def install
     # TODO: install script which chooses the right binary to download then
     # links them with bin.install
