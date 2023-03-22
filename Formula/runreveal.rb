5c9a41ea5f4dd8ed502ab9b2e602190162123e97
Alan Braithwaite <alan@runreveal.com>
2023-03-22 15:27:03 -0700

    upgrade runreveal

--- Formula/runreveal.rb
+++ Formula/runreveal.rb
@@ -1,9 +1,9 @@
 class Runreveal < Formula
   desc 'RunReveal CLI'
   homepage 'https://runreveal.com'
-  url 'https://github.com/runreveal/homebrew-runreveal/releases/download/runreveal-0.1.4/runreveal-v0.1.4-darwin-arm64.tar.gz'
-  sha256 '576f18dbb2dfbe155155432616b2f3bfe881d696e5133599b124a45b9b40d095'
-  version '0.1.4'
+  url 'https://github.com/runreveal/homebrew-runreveal/releases/download/runreveal-0.1.5/runreveal-v0.1.5-darwin-arm64.tar.gz'
+  sha256 '9633b877166cf6d358a5fb5c308eec260f721544c49917bf78fdeca44189d32f'
+  version '0.1.5'
   def install
     # TODO: install script which chooses the right binary to download then
     # links them with bin.install
